59453290cf7e0bc949e6080dab66918f1ba3d37f
Tshepo Sepatake <2004sepatake@gmail.com>
2025-11-21 00:54:18 +0200

    Adding StudentManagementSystem

---
+++ StudentManagementSystem/Student.java
@@ -0,0 +1,26 @@
+public class Student  {
+
+private int id ;
+private String name ;
+private int Age ;
+private String Email ;
+private String Course ;
+
+public Student (int id , String name , int Age , String Email , String Course ){
+
+
+this.id = id ;
+this.name = name ;
+this.Age = Age ;
+this.Email = Email ;
+this.Course = Course ;
+
+}
+
+public int getId (){return id;};
+public String getName (){return name ;} 
+public int getAge (){return Age ;}
+public String getEmail (){return Email ;}
+public String getCourse (){ return Course ;}
+
+}
